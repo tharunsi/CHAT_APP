ca86fd3ab29db6ab74a3a4aaf7c7d0a6d8386787
tharunsi Name <tharunsitharunsi@gmail.com>
2025-10-05 22:36:33 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,16 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-
+import path from 'path';
 
 
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
+
 
 // app.get("/",(req,res) => {
 //     res.send("Hello world")
@@ -25,6 +28,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist" , "index.html"))
+})
 
 server.listen(PORT, () =>{
     connectToMongoDB();
