90c2de83a63f2e85c50077235803dc266b973b1e
tharunsi Name <tharunsitharunsi@gmail.com>
2025-10-06 23:35:01 +0530

    MAIN

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["https://pingup-sandy.vercel.app/"],
+        origin:["https://pingup-sandy.vercel.app"],
         methods:["GET","POST"]
     }
 })
