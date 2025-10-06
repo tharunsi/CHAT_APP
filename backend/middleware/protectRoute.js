60a0784b293c8e5114b1ea8a247ed9c1951ac383
tharunsi Name <tharunsitharunsi@gmail.com>
2025-10-07 00:24:24 +0530

    commit

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,11 @@ import User from '../models/user.model.js';
 
 const protectRoute = async (req, res , next) => {
     try{
-        const token = req.cookies.jwt;
+        let token = req.cookies?.jwt;
+
+         if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
 
         if(!token) {
             return res.status(401).json({ error: "Unauthorized - No token provided"});
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge : 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite:  process.env.NODE_ENV === "production" ? "None" : "Strict",
-        secure: process.env.NODE_ENV === "production"
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development"
     })
 }
 
