884d9bcae7b8011ab35639c8d50bf14c4a96c739
tharunsi Name <tharunsitharunsi@gmail.com>
2025-10-06 22:47:38 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,16 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-import path from 'path';
+// import path from 'path';
 
 
 
 dotenv.config();
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
+
+ const PORT = process.env.PORT || 5000;
 
 
 // app.get("/",(req,res) => {
@@ -29,13 +30,15 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname,"frontend", "dist" , "index.html"))
-})
+// app.get("*", (req,res) => {
+//     res.sendFile(path.join(__dirname,"frontend", "dist" , "index.html"))
+// })
 
 server.listen(PORT, () =>{
     connectToMongoDB();
      console.log(`server running in port ${PORT}`)
     });
+
+    
