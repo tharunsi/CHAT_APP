cc0cfc8ad9f55ad8a055b8023acf18da2f949a12
tharunsi Name <tharunsitharunsi@gmail.com>
2025-10-07 00:04:49 +0530

    cors1

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,23 +1,35 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import express from 'express';
+import cors from 'cors';
 
 
 const app = express();
 
 const server = http.createServer(app);
+
+app.use(cors({
+   origin:["http://localhost:5173",
+            "https://pingup-sandy.vercel.app"],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 const io = new Server(server,{
     cors:{
         origin:["http://localhost:5173",
             "https://pingup-sandy.vercel.app"],
-        methods:["GET","POST"],
-  credentials: true
+        methods:["GET","POST","PUT","DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
     }
 })
 
 export const getReceiverSocketId = (receiverId) => {
 	return userSocketMap[receiverId];
 };
+
+
 
 const userSocketMap = {};// {userID: socketId}
 
